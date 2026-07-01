6d71fe9ca19c862819b317e30bbd4ed69c46a37b
shamalem <sham.alem@campus.technion.ac.il>
2026-07-01 14:58:04 +0300

    Create server.c

---
+++ src/server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+int main(void) {
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    printf("Socket created: fd=%d\n", listen_fd);
+    close(listen_fd);
+    return 0;
+}
